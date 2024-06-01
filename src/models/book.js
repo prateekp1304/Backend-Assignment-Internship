abb6b0230412a8da53612982ff914406759dca4e
Prateek Patel <prateekpatel307@gmail.com>
2024-06-01 20:24:41 +0530

    create book model

---
+++ src/models/book.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
